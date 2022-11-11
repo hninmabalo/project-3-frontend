bff465594ca48fb2874865417ea55f631f01dfb9
hninmabalo <hninmabalo@gmail.com>
2022-11-10 21:23:57 -0800

    add: profileContainer

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ProfileContainer from './ProfileContainer';
 
 const Welcome = () => {
     return (
         <div>
-            <h1>MERN Authentication</h1>
             <p>Welcome to my app.</p>
+            <ProfileContainer />
         </div>
     )
 }
